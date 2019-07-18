5c977c584257ad7d0460c7adb46da7bb140c5c72
Daniel Berryman <danielberryman90@gmail.com>
2019-07-18 11:46:15 -0400

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        Composition
+
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B", "Dell", "240", dimensions);
+
+        Monitor monitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        MotherBoard motherBoard = new MotherBoard("BJ-200","Asus",4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, monitor, motherBoard);
+        thePC.getMonitor().drawPixelAt(1500,1200,"red");
+        thePC.getMotherBoard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+    }
+}
